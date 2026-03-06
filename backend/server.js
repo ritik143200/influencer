0238327c89b2d4f7b9d95bc532099fe46d1ab3a2
vinit904 <vinitanjana904@gmail.com>
2026-03-06 15:52:58 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ const connectDB = require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
